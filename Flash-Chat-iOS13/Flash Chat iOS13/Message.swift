760c97b5498ef7f1e08f96034d38df958e2a13cb
Michael Adair <mjadair91@gmail.com>
2020-08-13 11:13:51 +0100

    added todo app

---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Adair on 12/08/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import Foundation
---
+++ Flash-Chat-iOS13/Flash Chat iOS13/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS13
+//
+//  Created by Michael Adair on 12/08/2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
